e126790999d92cad9a44d17f081c9e33c9da28cc
Ramel Henderson <boomboom3k@gmail.com>
2016-11-01 19:19:01 -0400

    Created ramel class

---
+++ src/com/company/Ramel.java
@@ -0,0 +1,16 @@
+package com.company;
+
+/**
+ * Created by Ramel Henderson
+ * on 11/1/2016.
+ */
+public class Ramel {
+    String name ="Ramel Henderson";
+    public String getName(){
+        return name;
+    }
+
+    public void sayHello (String name){
+        System.out.println("Hello " + name);
+    }
+}
