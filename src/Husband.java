72d4852e32e0bc6ac807ffe048e0f5201121eedf
Ramel Henderson <boomboom3k@gmail.com>
2016-11-01 20:15:17 -0400

    created hubby

---
+++ src/Husband.java
@@ -0,0 +1,33 @@
+/**
+ * Created by Ramel Henderson
+ * on 11/1/2016.
+ */
+public class Husband {
+    String firstname;
+    String lastname;
+    int yearborn;
+
+    public String getFirstname() {
+        return firstname;
+    }
+
+    public void setFirstname(String firstname) {
+        this.firstname = firstname;
+    }
+
+    public String getLastname() {
+        return lastname;
+    }
+
+    public void setLastname(String lastname) {
+        this.lastname = lastname;
+    }
+
+    public int getYearborn() {
+        return yearborn;
+    }
+
+    public void setYearborn(int yearborn) {
+        this.yearborn = yearborn;
+    }
+}
