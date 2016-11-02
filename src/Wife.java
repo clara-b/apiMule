8948624b2230dc1584aaabe44b69cd8d3ff333d3
clara-b <burgosclara@yahoo.com>
2016-11-01 20:15:27 -0400

    Wife

---
+++ src/Wife.java
@@ -0,0 +1,44 @@
+/**
+ * Created by claraburgos on 11/1/16.
+ */
+public class Wife {
+
+    String firstName;
+    String maidenName;
+    String lastName;
+    int yearBorn;
+    String
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getMaidenName() {
+        return maidenName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public int getYearBorn() {
+        return yearBorn;
+
+    }
+
+    public void setFirstName(String newFirstName){
+        firstName = newFirstName;
+    }
+
+    public void setLastName(String newLastName){
+        lastName = newLastName;
+    }
+
+    public void setMaidenName(String newMaidenName){
+        maidenName = newMaidenName;
+    }
+
+    public void setYearBorn(int newYearBorn){
+        yearBorn = newYearBorn;
+    }
+}
