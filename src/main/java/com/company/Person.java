2fe769e46bc326f16bce1c49515ac60d49ececcb
clara-b <burgosclara@yahoo.com>
2016-11-15 14:20:29 -0500

    abstract person class

---
+++ src/main/java/com/company/Person.java
@@ -0,0 +1,11 @@
+package com.company;
+
+/**
+ * Created by claraburgos on 11/15/16.
+ */
+public abstract class Person {
+    String firstName;
+    String lastName;
+    String gender;
+    int yearBorn;
+}
