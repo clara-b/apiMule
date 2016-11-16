c31a997bb45ed47a5c4ff310e161468988486e3a
clara-b <burgosclara@yahoo.com>
2016-11-15 21:06:09 -0500

    getters and setters

--- src/main/java/com/clarabee/apimule/Person.java
+++ src/main/java/com/clarabee/apimule/Person.java
@@ -21,13 +21,21 @@ public abstract class Person {
     }
 
     //getter for lastName
+    public String getLastName(){ return lastName; }
+
     //setter for lastName
+    public void setLastName(String lastName) { this.lastName = lastName; }
 
     //getter for gender
+    public String getGender() { return gender; }
+
     //setter for gender
+    public void setGender(String gender) { this.gender = gender; }
 
     //getter for yearBorn
-    //setter for yearBorn
+    public int getYearBorn(){ return yearBorn; }
 
+    //setter for yearBorn
+    public void setYearBorn(int yearBorn) { this.yearBorn = yearBorn; }
 
 }
