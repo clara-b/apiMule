73c0f75029b7a5e9bc14a95def74a874ece062ef
clara-b <burgosclara@yahoo.com>
2016-11-15 23:54:08 -0500

    printsInfo method

--- src/main/java/com/clarabee/apimule/Person.java
+++ src/main/java/com/clarabee/apimule/Person.java
@@ -38,7 +38,7 @@ public abstract class Person {
     //setter for yearBorn
     public void setYearBorn(int yearBorn) { this.yearBorn = yearBorn; }
 
-    public void printsInfo() {
+    public void printInfo() {
         System.out.println("Name: " + firstName + lastName);
         System.out.println("Gender: " + gender);
         System.out.println("Year of Birth : " + yearBorn);
